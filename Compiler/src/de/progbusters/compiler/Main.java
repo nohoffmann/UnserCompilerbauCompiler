426a6ab549ba20ab864e4622f1e809f6752f8603
nohoffmann <43476030+nohoffmann@users.noreply.github.com>
2018-10-02 13:50:42 +0200

    Main: createBytecode()
    createBytecode-Methode hinzugefügt, automatischer Aufruf von Jasmin und damit Generierung von .class-Dateien als Ausgabe des Compilers

--- Compiler/src/de/progbusters/compiler/Main.java
+++ Compiler/src/de/progbusters/compiler/Main.java
@@ -2,8 +2,13 @@ package de.progbusters.compiler;
 
 import java.io.BufferedWriter;
 import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.StringReader;
 import java.io.Writer;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
@@ -12,24 +17,27 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 import de.progbusters.parser.ArithmeticLexer;
 import de.progbusters.parser.ArithmeticParser;
+import jasmin.ClassFile;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
 		String inputFile = ""/* "basic.maths"*/;
-		String outputFile = "./tpblcOut.j";
+	//	String outputFile = "./tpblcOut.j";
+				
 		
 		 for (String s: args) {
 	            inputFile = s;
 	        }
 		
 		CharStream input = CharStreams.fromFileName(inputFile);
+		//writeJasminCodeToFile(compile(input), outputFile);
 		
+		createBytecode(input);
 		
 		/*obere Zeile schreibt auf Standardausgabe
 		  untere Zeile schreibt in Datei outputFile*/
 		//System.out.println(compile(input));
-		writeJasminCodeToFile(compile(input), outputFile);
-		Runtime.getRuntime().exec("java -jar ../../../../lib/jasmin.jar " + outputFile);
+		
 	}
 	
 	//startet lexer, parser und visitor
@@ -52,6 +60,25 @@ public class Main {
 				instructions;
 	}
 
+	
+	static void createBytecode(CharStream inputStream) throws Exception {
+		//setzt outputDir zu aktuellem verzeichnis
+		Path outputDir = FileSystems.getDefault().getPath("./");
+		
+		//generieren von JasminCode
+		String code = compile(inputStream);
+		//Jasmin-Aufruf
+		ClassFile classFile = new ClassFile();
+		classFile.readJasmin(new StringReader(code), "", false);
+		
+		//.class datei schreiben
+		Path outputPath = outputDir.resolve(classFile.getClassName() + ".class");
+		try(OutputStream out = Files.newOutputStream(outputPath)) {
+			classFile.write(out);
+		}
+	}
+
+	
 	//simple Hilfsfunktion, die den String jasminCode in die Datei filename schreibt
 	static void writeJasminCodeToFile(String jasminCode, String filename) throws Exception {
 		Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), "utf-8")); 
@@ -59,5 +86,5 @@ public class Main {
 		writer.close();
 		return;
 	}
-	
+
 }
